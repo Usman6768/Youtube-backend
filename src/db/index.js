0b85eb97bfb4dc6877a0bb641462e7ff17460035
Usman Pervez <usmanpervez5@gmail.com>
2025-01-10 20:33:19 +0500

    Added Code in Utils

--- src/db/index.js
+++ src/db/index.js
@@ -5,11 +5,10 @@ const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n MongoDB connected !!! DB HOST : ${connectionInstance.connection.host}` );
-        
+
     } catch (error) {
         console.log("MONGODB ERROR ", error);
         process.exit(1)
-        
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -6,3 +6,11 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("Mongo DB connection failed !!! " , error);
+})
